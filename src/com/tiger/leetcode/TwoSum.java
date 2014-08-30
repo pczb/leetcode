42349eac4ba139e7115c8f49e390baa84e39f098
pczb00 <pczb@163.com>
2014-08-30 11:06:10 +0800

    solve TwoSum

---
+++ src/com/tiger/leetcode/TwoSum.java
@@ -0,0 +1,32 @@
+package com.tiger.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * Created by tigerzhang on 14/8/30.
+ */
+public class TwoSum {
+    public int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(numbers.length);
+        int[] res = new int[2];
+        int index = 1;
+        for(int num: numbers){
+            int need = target - num;
+            if(map.containsKey(num)){
+                res[0] = map.get(num);
+                res[1] = index;
+                break;
+            }
+            map.put(need, index);
+            index += 1;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TwoSum sum = new TwoSum();
+        int[] res = sum.twoSum(new int[]{1, 2, 3}, 3);
+        Util.printIntArray(res);
+    }
+}
---
+++ src/com/tiger/leetcode/Util.java
@@ -0,0 +1,20 @@
+package com.tiger.leetcode;
+
+/**
+ * Created by tigerzhang on 14/8/30.
+ */
+public class Util {
+    public static <K> void printArray(K[] array){
+        for(K val: array) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+    public static void printIntArray(int[] array){
+        for(int val: array) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
+}
