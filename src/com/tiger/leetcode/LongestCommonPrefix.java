dcb3bca2ac66723d8c12a39acefa5f4c4e277487
pczb00 <pczb@163.com>
2014-08-30 21:22:16 +0800

    Solve 3_Sum

---
+++ src/com/tiger/leetcode/LongestCommonPrefix.java
@@ -0,0 +1,21 @@
+package com.tiger.leetcode;
+
+/**
+ * Created by tigerzhang on 14/8/30.
+ */
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs == null) return "";
+        String result = "";
+        int len = strs.length;
+        int i, j;
+        if (len == 0) return "";
+        for (j = 0; ; ++j) {
+            for (i = 0; i < len; ++i)
+                if (j >= strs[i].length() || strs[i].charAt(j) != strs[0].charAt(j)) break;
+            if (i < len) break;
+            result += strs[0].charAt(j);
+        }
+        return result;
+    }
+}
